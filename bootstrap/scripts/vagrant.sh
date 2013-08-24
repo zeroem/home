0ae493d3855dfb5bed3066854c6eeb9cd7afc8d2
Darrell Hamilton <darrell.noice@gmail.com>
2013-08-23 23:11:28 -0700

    vagrant installation script

---
+++ bootstrap/scripts/vagrant.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+PLUGINS=(vagrant-lxc)
+
+which vagrant > /dev/null
+
+if [ $? -ne 0 ]
+then
+  wget -P /tmp/ "http://files.vagrantup.com/packages/7ec0ee1d00a916f80b109a298bab08e391945243/vagrant_1.2.7_i686.deb"
+  sudo dpkg -i /tmp/vagrant_1.2.7_i686.deb
+fi
+
+INSTALLED_PLUGINS=`vagrant plugin list`
+
+for PLUGIN in "${PLUGINS[@]}"
+do
+  echo "${INSTALLED_PLUGINS}" | grep -q "${PLUGIN}"
+
+  if [ $? -ne 0 ]
+  then
+    vagrant plugin install ${PLUGIN}
+  fi
+done
