f558e43739f5420d3badc6d52afcf9ccadcd03cb
Darrell Hamilton <darrell.noice@gmail.com>
2013-10-02 23:26:09 -0700

    optimize 're-install' of vagrant

--- bootstrap/scripts/vagrant.sh
+++ bootstrap/scripts/vagrant.sh
@@ -7,7 +7,7 @@ which vagrant > /dev/null
 if [ $? -ne 0 ]
 then
   wget -P /tmp/ "http://files.vagrantup.com/packages/7ec0ee1d00a916f80b109a298bab08e391945243/vagrant_1.2.7_i686.deb"
-  sudo dpkg -i /tmp/vagrant_1.2.7_i686.deb
+  sudo dpkg --skip-same-version -i /tmp/vagrant_1.2.7_i686.deb
 fi
 
 INSTALLED_PLUGINS=`vagrant plugin list`
