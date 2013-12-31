0c5d3689e78398a4db98f1d0114f922458259e71
Darrell Hamilton <darrell.noice@gmail.com>
2013-12-30 16:34:00 -0800

    upgraded vagrant to 1.4.1

--- bootstrap/scripts/vagrant.sh
+++ bootstrap/scripts/vagrant.sh
@@ -2,17 +2,13 @@
 
 PLUGINS=(vagrant-lxc vagrant-hostmanager)
 
-wget -P /tmp/ "http://files.vagrantup.com/packages/a40522f5fabccb9ddabad03d836e120ff5d14093/vagrant_1.3.5_x86_64.deb"
-sudo dpkg --skip-same-version -i /tmp/vagrant_1.3.5_x86_64.deb
+wget -P /tmp/ "https://dl.bintray.com/mitchellh/vagrant/vagrant_1.4.1_x86_64.deb"
+
+sudo dpkg --skip-same-version -i /tmp/vagrant_1.4.1_x86_64.deb
 
 INSTALLED_PLUGINS=`vagrant plugin list`
 
 for PLUGIN in "${PLUGINS[@]}"
 do
-  echo "${INSTALLED_PLUGINS}" | grep -q "${PLUGIN}"
-
-  if [ $? -ne 0 ]
-  then
-    vagrant plugin install ${PLUGIN}
-  fi
+  vagrant plugin install ${PLUGIN}
 done
